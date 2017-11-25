3839ad38b2d38ccdea9721f5fc1aae558d11eeef
seandgrant <seandgrant@gmail.com>
2017-11-25 22:24:38 +1100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import Routes from './components/Routes/Routes';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Tennisity</h1>
-        </header>
-        <p className="App-intro" />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="ui container">
+    <Menu secondary>
+      <Menu.Item>
+        <Link to="/">Home</Link>
+      </Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Link to="/login">Login</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/signup">Sign up</Link>
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+    <Routes />
+  </div>
+);
 
 export default App;
