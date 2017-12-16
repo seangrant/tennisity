9b9d2c155e7b62264050e52e807a3162880ff78d
seandgrant <seandgrant@gmail.com>
2017-12-16 21:47:13 +1100

    wip

--- src/components/Team/AddTeam/AddTeam.js
+++ src/components/Team/AddTeam/AddTeam.js
@@ -16,25 +16,38 @@ class AddTeam extends Component {
   static propTypes = {
     clubs: PropTypes.shape({
       allClubs: PropTypes.array
+    }).isRequired,
+    teams: PropTypes.shape({
+      teams: PropTypes.array,
+      refetch: PropTypes.func
     }).isRequired
   };
 
-  renderSelect = ({ input, clubs }) => {
-    const options = clubs.map(club => ({
+  renderClubSelect = ({ input, options }) => (
+    <Select value={input.value} options={options} onChange={input.onChange} />
+  );
+
+  renderTeamSelect = ({ input, options }) => (
+    <Select value={input.value} options={options} onChange={input.onChange} />
+  );
+
+  render() {
+    console.log({ props: this.props });
+    const {
+      clubs: { allClubs = [] },
+      teams: { teams = [], refetch }
+    } = this.props;
+
+    const clubOptions = allClubs.map(club => ({
       value: club.code,
       label: club.name
     }));
 
-    return (
-      <Select value={input.value} options={options} onChange={input.onChange} />
-    );
-  };
+    const teamOptions = teams.map(team => ({
+      value: team.id,
+      label: team.name
+    }));
 
-  render() {
-    console.log({ props: this.props });
-    const { clubs: { allClubs = [] }, teams: { teams } } = this.props;
-
-    console.log({ allClubs, teams });
     return (
       <Card>
         <Form>
@@ -43,21 +56,43 @@ class AddTeam extends Component {
             <Field
               id="club"
               name="club"
-              component={this.renderSelect}
+              component={this.renderClubSelect}
               type="text"
               placeholder="Club Name"
-              clubs={allClubs}
+              options={clubOptions}
             />
           </Form.Field>
           <Form.Field>
-            <label htmlFor="teamName">Club Name</label>
+            <label htmlFor="clubName">Club Name</label>
             <Field
-              id="teamName"
-              name="teamName"
+              id="clubName"
+              name="clubName"
               component="input"
               type="text"
               placeholder="Team Name"
             />
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="teamName">Team Name</label>
+            <Field
+              id="teamName"
+              name="teamName"
+              component={this.renderTeamSelect}
+              type="text"
+              placeholder="Club Name"
+              options={teamOptions}
+            />
+          </Form.Field>
+          <Form.Field>
+            <button
+              onClick={() =>
+                refetch({
+                  category: 2
+                })
+              }
+            >
+              RE-do
+            </button>
           </Form.Field>
         </Form>
       </Card>
@@ -69,11 +104,14 @@ const clubQuery = graphql(ClubQuery, { name: 'clubs' });
 
 const categoryTeamsQuery = graphql(TeamQuery, {
   name: 'teams',
-  options: ({ match }) => ({
-    variables: {
-      category: match.params.category
-    }
-  })
+  options: ({ match }) => {
+    console.log('refetch');
+    return {
+      variables: {
+        category: match.params.category
+      }
+    };
+  }
 });
 const form = reduxForm({
   form: formName
